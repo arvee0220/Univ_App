dcc4490de50379e94a8297a03964ea362f21cbef
arvee0220 <rowellverdejo@gmail.com>
2024-06-20 22:28:17 -0400

    Push java exercise

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,17 +1,28 @@
 package main;
 
+import university.Course;
+import university.Enrollment;
+import university.Student;
+
 public class Main {
     public static void main(String[] args) {
+       Course course1 = new Course();
+       Student student1 = new Student();
+       Enrollment enrollment = new Enrollment();
 
-        if (args.length > 0) {
-            System.out.println("Command-line arguments:");
-            // Print each argument
-            for (int i = 0; i < args.length; i++) {
-                System.out.println("args[" + i + "] = " + args[i]);
-            }
-        } else {
-            System.out.println("No command-line arguments provided.");
-        }
+
+       student1.studentId = 1236;
+       student1.name = "Linus Torvalds";
+
+       enrollment.course = course1;
+       enrollment.student = student1;
+
+       course1.courseId = 1;
+       course1.courseName = "Data Structures & Algorithm";
+
+
+       System.out.println("Student Information: " + "Student ID: " + enrollment.student.studentId + " Student Name: " + enrollment.student.name);
+       System.out.println("Course details: " + "Course ID: " + enrollment.course.courseId + " Course Name: " + enrollment.course.courseName);
 
     }
 }
